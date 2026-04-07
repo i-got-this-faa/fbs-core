71c71d4f747ece730bc60249d41108f1c62f72d9
Radhey Kalra <radheykalra901@gmail.com>
2026-04-07 23:18:46 +0530

    Revert "Added core functionality of core storage engine"

--- go.mod
+++ go.mod
@@ -5,5 +5,4 @@ go 1.26.1
 require (
 	github.com/go-chi/chi/v5 v5.2.5 // indirect
 	github.com/go-chi/cors v1.2.2 // indirect
-	github.com/google/uuid v1.6.0 // indirect
 )
--- internal/storage/error.go
+++
@@ -1,9 +0,0 @@
-package storage 
-
-import "errors"
-
-var (
-	ErrNotFound      = errors.New("storage: file not found")
-	ErrInvalidKey    = errors.New("storage: invalid object key")
-	ErrPathTraversal = errors.New("storage: path traversal detected")
-)
